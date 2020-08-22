f167b9ab92e70d051597aad3309843aa32544b2e
Joaquin <jbetz@fi.uba.ar>
2020-08-22 04:53:01 -0300

    agrego intento de poner musica en el app

--- src/main/java/edu/fiuba/algo3/App.java
+++ src/main/java/edu/fiuba/algo3/App.java
@@ -2,9 +2,16 @@ package edu.fiuba.algo3;
 
 import edu.fiuba.algo3.interfaz.layouts.LayoutInicio;
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
+import javafx.scene.media.AudioClip;
+import javafx.scene.media.MediaPlayer;
 import javafx.stage.Stage;
+import javafx.scene.media.Media;
+
+import java.io.File;
+import java.nio.file.Paths;
 
 /**
  * JavaFX App cC
@@ -21,8 +28,20 @@ public class App extends Application {
         scene = new Scene(layoutInicio, 640, 480);
 
         mostrarStage(stage);
+        //tocarMusica();
     }
+    private void tocarMusica(){
+        //opcion con audioclip
+        //AudioClip audioClip = new AudioClip(Paths.get("src/musica/kahoot.mp3").toUri().toString());
+        //audioClip.play();
 
+        //opcion con media
+        /*String path = "src/musica/kahoot.mp3";
+        Media media = new Media(new File(path).toURI().toString());
+        MediaPlayer mediaPlayer = new MediaPlayer(media);
+        mediaPlayer.setAutoPlay(true);*/
+
+    }
     private void mostrarStage(Stage stage) {
 
         stage.setTitle("Algohoot!");
